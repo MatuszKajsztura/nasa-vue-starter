6bc13a2f58d5cfff51ea55ecc0dd60b65516f1c4
Mateusz Kajsztura <mkajsztura@davinci-studio.eu>
2019-01-11 15:29:17 +0100

    add search view + basic input style

---
+++ src/views/Search.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="wrapper">
+    <div class="search">
+      <label>Search</label>
+      <input name="search">
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+  name: 'search',
+};
+</script>
+<style lang="scss" scoped>
+  .wrapper {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 0;
+    padding: 30px;
+    width: 100%;
+
+    .search {
+      width: 300px;
+      display: flex;
+      flex-direction: column;
+
+      input {
+        height: 50px;
+        border: 0;
+        border-bottom: 2px #000 solid;
+
+        &:focus {
+          outline: none;
+        }
+      }
+    }
+  }
+
+</style>
